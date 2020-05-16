bba6c95bbf9694d6624937209d001a5b3aa5257a
Dybala-Drgon <59010060+Dybala-Drgon@users.noreply.github.com>
2020-05-16 23:46:34 +0800

    Update staff.cpp

--- c++大作业/高校薪资管理系统C++/staff.cpp
+++ c++大作业/高校薪资管理系统C++/staff.cpp
@@ -1,89 +1,172 @@
-#pragma once
-class staff	   //普通职员类
+#include "staff.h"
+#include<cstdio>
+#include<iostream>
+using namespace std;
+
+
+
+void staff::Setinfo()
 {
-private:
-	char ID[6];
-	char Name[21];
-	double Basic_salary;
-	double Salary_sum;
-	char pos[10] = { "普通职员" };
-public:									
-	void Setinfo();
-	virtual void show();//设置虚函数
-	char* GetID();
-	char* GetName();	
-	inline
-		double GetBasic_salary()const;
-	inline
-		double GetSalary_sum()const;	//设置成内联函数节省空间
-	void setsalary(double basic, double classhour, double exper, double admin);		 //计算薪资
-	staff* next;//使用链表进行增删改查
-	int num;//总数
-};
-	 
+	cout << "输入id" << endl;
+	cin >> ID;
+	getchar();//吃掉回车
+	cout << "输入name" << endl;
+	cin >> Name;
+	getchar();
+	cout << "输入基本工资" << endl;
+	cin >> Basic_salary;
+	Salary_sum = Basic_salary;//基础职员只有基本工资
+}//设置职员
 
+void staff::show()
+{
+	cout << "ID\t\t" << "NAME\t" << "基础工资\t" << "工资总和\t" <<"类别"<< endl;
+	//使用printf方便输出
+	printf("%-6s\t\t%s\t%-8.2f\t%-8.2f\t%s\n", ID, Name, Basic_salary, Salary_sum,pos);
+}
 
-//这是教师类	使用虚基类继承
-class Teacher :virtual public staff {	 
-public:
-	 virtual void show();//设置虚函数，实现多态
-	 void Setinfo();	//重写函数，实现多态
-	 void Setclass_hour_salary();
-	 double get_class_hour_salary();
-	 Teacher* next;//使用链表进行增删改查
-	 int num;//总数
-	 Teacher() { next = nullptr; }
-private: 
-	double class_hour_salary;//课时补助
-	 char pos[10] = { "任课教师" };
-};	  
+char* staff::GetID()
+{
+	return ID;
+}
 
-//这是实验员类	   虚基类继承
-class Experimenter :virtual public staff {
-public:
-	virtual void show();//设置虚函数，实现多态
-	void Setinfo();		//重写函数，实现多态
-	void Setexper_subsidy();
-	double get_exper_subsidy();
-	int num;//总数
-	Experimenter* next;//使用链表进行增删改查
-	Experimenter() { next = nullptr; }
-private:
-	double exper_subsidy;//实验室补助
-	char pos[10] = { "实验员" };
-};
+char* staff::GetName()
+{
+	return Name;
+}
 
-//这是行政人员类	 虚基类继承
-class Admini :virtual public staff {
-public:
-	virtual void show();//设置虚函数，实现多态
-	void Setinfo();		//重写函数，实现多态
-	void Setadmin_subsidy();
-	double get_admin_subsidy();
-	Admini* next;//使用链表进行增删改查
-	int num;//总数
-	Admini() { next = nullptr; }
-private:
-	double admin_subsidy;//行政补助
-	char pos[10] = { "行政人员" };
-};
+inline double staff::GetBasic_salary() const
+{
+	return Basic_salary;
+}
 
-class Teacher_Experimenter :public Teacher, public Experimenter {
-public:
-	void show();   //没写virtual也默认是virtual类型
-	void Setinfo();
-	Teacher_Experimenter* next;//使用链表进行增删改查
-	int num;//总数
-	Teacher_Experimenter() { next = nullptr; }
-};
+inline double staff::GetSalary_sum() const
+{
+	return Salary_sum;
+}
 
-class Admini_Teacher :public Teacher, public Admini {
-public:
-	void show();   //没写virtual也默认是virtual类型
-	void Setinfo();
-	Admini_Teacher* next;//使用链表进行增删改查
-	Admini_Teacher() { next = nullptr; }
-	int num;//总数
-};
+void staff::setsalary(double basic, double classhour, double exper, double admin)
+{
+	Salary_sum = basic + classhour + exper+admin;
+}
 
-void showinfo(staff* ptr); 	   //实现虚函数作用
+void Teacher::show()
+{
+	cout << "ID\t" << "NAME\t" << "基础工资\t" << "工资总和\t" <<"课时补助\t"<< "类别" << endl;
+	//使用printf方便输出
+	printf("%-6s\t%s\t%-8.2f\t%-8.2f\t%-8.2f\t任课教师\n", GetID(), GetName(),GetBasic_salary(),GetSalary_sum(),class_hour_salary );
+}
+
+void Teacher::Setinfo()
+{
+	staff::Setinfo();
+	Setclass_hour_salary();
+	setsalary(GetBasic_salary(), class_hour_salary, 0, 0);
+	getchar();//吃掉回车
+}
+
+void Teacher::Setclass_hour_salary()
+{
+	cout << "课时补助" << endl;
+	cin >> class_hour_salary;
+	
+}
+
+double Teacher::get_class_hour_salary()
+{
+	return class_hour_salary;
+}
+
+void Experimenter::show()
+{
+	cout << "ID\t" << "NAME\t" << "基础工资\t" << "工资总和\t" << "实验室补助\t" << "类别" << endl;
+	//使用printf方便输出
+	printf("%-6s\t%s\t%-8.2f\t%-8.2f\t%-8.2f\t实验员\n", GetID(), GetName(), GetBasic_salary(), GetSalary_sum(), exper_subsidy);
+}
+
+void Experimenter::Setinfo()
+{
+	staff::Setinfo();
+	Setexper_subsidy();
+	setsalary(GetBasic_salary(), 0, exper_subsidy, 0);
+	getchar();//吃掉回车
+}
+
+void Experimenter::Setexper_subsidy()
+{
+
+	cout << "实验室补助" << endl;
+	cin >> exper_subsidy;
+}
+
+double Experimenter::get_exper_subsidy()
+{
+	return exper_subsidy;
+}
+
+void Admini::show()
+{
+	cout << "ID\t" << "NAME\t" << "基础工资\t" << "工资总和\t" << "行政补助\t" << "类别" << endl;
+	//使用printf方便输出
+	printf("%-6s\t%s\t%-8.2f\t%-8.2f\t%-8.2f\t行政人员\n", GetID(), GetName(), GetBasic_salary(), GetSalary_sum(), admin_subsidy);
+}
+
+void Admini::Setinfo()
+{
+	staff::Setinfo();
+	Setadmin_subsidy();
+	setsalary(GetBasic_salary(), 0, 0,admin_subsidy);
+	getchar();//吃掉回车
+}
+
+void Admini::Setadmin_subsidy()
+{
+	cout << "行政补助" << endl;
+	cin >> admin_subsidy;
+}
+
+double Admini::get_admin_subsidy()
+{
+	return admin_subsidy;
+}
+
+void Teacher_Experimenter::show()
+{
+	cout << "ID\t" << "NAME\t" << "基础工资\t" << "工资总和\t" << "课时和实验补助\t" << "  类别" << endl;
+	//使用printf方便输出
+	printf("%-6s\t%s\t%-8.2f\t%-8.2f\t%-7.2f和%-7.2f 教师兼实验员\n", GetID(), GetName(), GetBasic_salary(), GetSalary_sum(), get_class_hour_salary(),get_exper_subsidy());
+ }
+
+void Teacher_Experimenter::Setinfo()
+{
+	staff::Setinfo();
+	Setexper_subsidy();
+	Setclass_hour_salary();
+	setsalary(GetBasic_salary(), get_class_hour_salary(), get_exper_subsidy(), 0);
+	getchar();//吃掉回车
+}
+
+void Admini_Teacher::show()
+{
+	cout << "ID\t" << "NAME\t" << "基础工资\t" << "工资总和\t" << "课时和行政补助\t" << "  类别" << endl;
+	//使用printf方便输出
+	printf("%-6s\t%s\t%-8.2f\t%-8.2f\t%-7.2f和%-7.2f 教师兼行政人员\n", GetID(), GetName(), GetBasic_salary(), GetSalary_sum(), get_class_hour_salary(), get_admin_subsidy());
+
+}
+
+void Admini_Teacher::Setinfo()
+{
+	staff::Setinfo();
+	Setadmin_subsidy();
+	Setclass_hour_salary();
+	setsalary(GetBasic_salary(), get_class_hour_salary(), 0,get_admin_subsidy());
+	getchar();//吃掉回车
+
+}
+
+void showinfo(staff* ptr)
+{
+	
+		ptr->show();
+	
+}
