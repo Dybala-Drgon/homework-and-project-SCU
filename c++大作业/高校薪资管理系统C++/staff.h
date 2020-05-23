3d248f49beca13c7f4ed4b9adddfaa62dad5932c
Dybala-Drgon <59010060+Dybala-Drgon@users.noreply.github.com>
2020-05-23 19:02:31 +0800

    Update staff.h

--- c++大作业/高校薪资管理系统C++/staff.h
+++ c++大作业/高校薪资管理系统C++/staff.h
@@ -2,14 +2,14 @@
 class staff	   //普通职员类
 {
 private:
-	char ID[6];
+	char ID[7];
 	char Name[21];
 	double Basic_salary;
 	double Salary_sum;
 	char pos[10] = { "普通职员" };
 public:									
 	void Setinfo();
-	virtual void show();//设置虚函数
+	virtual void show();				//设置虚函数
 	char* GetID();
 	char* GetName();	
 	inline
@@ -26,64 +26,74 @@ public:
 //这是教师类	使用虚基类继承
 class Teacher :virtual public staff {	 
 public:
-	 virtual void show();//设置虚函数，实现多态
-	 void Setinfo();	//重写函数，实现多态
+	 virtual void show();			//设置虚函数，实现多态
+	 void Setinfo();				//重写函数，实现多态
 	 void Setclass_hour_salary();
 	 double get_class_hour_salary();
-	 Teacher* next;//使用链表进行增删改查
-	 int num;//总数
+	 Teacher* next;					//使用链表进行增删改查
+	 int num;						//总数
 	 Teacher() { next = nullptr; }
+	 void showall();
+	 void showmenu();
 private: 
-	double class_hour_salary;//课时补助
+	double class_hour_salary;		//课时补助
 	 char pos[10] = { "任课教师" };
 };	  
 
 //这是实验员类	   虚基类继承
 class Experimenter :virtual public staff {
 public:
-	virtual void show();//设置虚函数，实现多态
-	void Setinfo();		//重写函数，实现多态
+	virtual void show();		//设置虚函数，实现多态
+	void Setinfo();				//重写函数，实现多态
 	void Setexper_subsidy();
 	double get_exper_subsidy();
-	int num;//总数
-	Experimenter* next;//使用链表进行增删改查
+	int num;					//总数
+	Experimenter* next;			//使用链表进行增删改查
 	Experimenter() { next = nullptr; }
+	void showall();
+	void showmenu();
 private:
-	double exper_subsidy;//实验室补助
+	double exper_subsidy;		//实验室补助
 	char pos[10] = { "实验员" };
 };
 
 //这是行政人员类	 虚基类继承
 class Admini :virtual public staff {
 public:
-	virtual void show();//设置虚函数，实现多态
-	void Setinfo();		//重写函数，实现多态
+	virtual void show();		//设置虚函数，实现多态
+	void Setinfo();				//重写函数，实现多态
 	void Setadmin_subsidy();
 	double get_admin_subsidy();
-	Admini* next;//使用链表进行增删改查
-	int num;//总数
+	Admini* next;				//使用链表进行增删改查
+	int num;					//总数
 	Admini() { next = nullptr; }
+	void showall();
+	void showmenu();
 private:
-	double admin_subsidy;//行政补助
+	double admin_subsidy;		//行政补助
 	char pos[10] = { "行政人员" };
 };
 
 class Teacher_Experimenter :public Teacher, public Experimenter {
 public:
-	void show();   //没写virtual也默认是virtual类型
+	void show();				 //没写virtual也默认是virtual类型
 	void Setinfo();
 	Teacher_Experimenter* next;//使用链表进行增删改查
-	int num;//总数
+	int num;					//总数
 	Teacher_Experimenter() { next = nullptr; }
+	void showall();
+	void showmenu();
 };
 
 class Admini_Teacher :public Teacher, public Admini {
 public:
-	void show();   //没写virtual也默认是virtual类型
+	void show();				//没写virtual也默认是virtual类型
 	void Setinfo();
-	Admini_Teacher* next;//使用链表进行增删改查
+	Admini_Teacher* next;		//使用链表进行增删改查
 	Admini_Teacher() { next = nullptr; }
 	int num;//总数
+	void showall();
+	void showmenu();
 };
 
 void showinfo(staff* ptr); 	   //实现虚函数作用
